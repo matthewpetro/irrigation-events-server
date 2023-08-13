b214f757ef3ca1ec07f7670deb90b2bb0e112b3e
Matthew Petro <matthew.petro@aa.com>
2023-08-12 22:23:36 -0700

    Moved types to a types file

---
+++ types.ts
@@ -0,0 +1,18 @@
+import { MaybeDocument } from 'nano'
+
+const DeviceState = {
+  ON: 'on',
+  OFF: 'off',
+} as const
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+type DeviceState = (typeof DeviceState)[keyof typeof DeviceState]
+
+interface IrrigationEventDocument extends MaybeDocument {
+  _id: string
+  deviceName: string
+  deviceId: number
+  state: DeviceState
+}
+
+export { DeviceState, IrrigationEventDocument }
