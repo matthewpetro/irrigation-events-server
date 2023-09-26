7abbba7d48d803c7e22e7f5e9f6108a28e416810
Matthew Petro <matthew.petro@aa.com>
2023-09-25 17:06:50 -0700

    Use new Warning type

--- src/viewmodels/irrigationEvent.ts
+++ src/viewmodels/irrigationEvent.ts
@@ -3,6 +3,7 @@ import {
   DeviceState,
   IrrigationEventDocument,
   IrrigationEventViewModel,
+  Warning,
 } from '../types.js'
 
 const roundTimestampToMinute = (timestamp: string): string =>
@@ -36,30 +37,29 @@ function createViewmodelsFromDeviceEvents(
         startDate: roundTimestampToMinute(event._id),
         title: event.deviceName,
         deviceId: event.deviceId,
+        warning: Warning.MISSING_OFF,
       })
       i += 1
     } else if (event.state === DeviceState.ON && !nextEvent) {
-      // ON followed by nothing means the final OFF event is missing
-      // or the device is still on
-      // TODO: handle this situation better
-      // Maybe check to see if the current time is within the
-      // time range being searched for? If so, assume the device
-      // is still on, if not, assume the final OFF event is missing
-      // and do something to indicate that.
+      // ON followed by nothing probably means the device is currently on.
+      // It may also mean that the final OFF event is missing, but
+      // it's more likely that the device is still on.
       viewmodels.push({
         // eslint-disable-next-line no-underscore-dangle
         startDate: roundTimestampToMinute(event._id),
         title: event.deviceName,
         deviceId: event.deviceId,
+        warning: Warning.CURRENTLY_ON,
       })
       i += 1
     } else if (event.state === DeviceState.OFF) {
       // OFF means an ON event is missing
       viewmodels.push({
         // eslint-disable-next-line no-underscore-dangle
-        endDate: roundTimestampToMinute(event._id),
+        startDate: roundTimestampToMinute(event._id),
         title: event.deviceName,
         deviceId: event.deviceId,
+        warning: Warning.MISSING_ON,
       })
       i += 1
     }
