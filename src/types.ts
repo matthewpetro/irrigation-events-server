04ac19e6564b4e3210fe011246101f0125529023
Matthew Petro <matthew.petro@aa.com>
2023-09-25 17:06:21 -0700

    Added Warning type

--- src/types.ts
+++ src/types.ts
@@ -22,12 +22,22 @@ type IrrigationEvent = {
   state: DeviceState
 }
 
+const Warning = {
+  MISSING_ON: 'The ON event is missing. The time shown is the OFF time.',
+  MISSING_OFF: 'The OFF event is missing. The time shown is the ON time.',
+  CURRENTLY_ON: 'The device is currently ON.',
+} as const
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+type Warning = (typeof Warning)[keyof typeof Warning]
+
 // Conforms to AppointmentModel from @devexpress/dx-react-scheduler
 export type IrrigationEventViewModel = {
   startDate?: string
   endDate?: string
   title: string
   deviceId: number
+  warning?: Warning
 }
 
-export { DeviceState, IrrigationEventDocument, IrrigationEvent }
+export { DeviceState, IrrigationEventDocument, IrrigationEvent, Warning }
