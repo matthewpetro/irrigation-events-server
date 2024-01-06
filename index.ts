1d4fd16c30c5f6f9371968ebb7700f987ac14ca9
Matthew Petro <matthew.petro@aa.com>
2024-01-05 22:20:03 -0700

    Move stuff to app.js

--- index.ts
+++ index.ts
@@ -1,21 +1,6 @@
-import express, { Express, Request, Response } from 'express'
-import cors from 'cors'
-import sendEventToDb from './src/middleware/sendEventToDb.js'
-import getIrrigationEvents from './src/middleware/getIrrigationEvents.js'
+import app from './src/app.js'
 
 const PORT = 3000
-
-const app: Express = express()
-app.use(express.json())
-app.use(cors())
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('OK')
-})
-
-app.post('/irrigationEvent', sendEventToDb)
-
-app.get('/irrigationEvents', getIrrigationEvents)
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
