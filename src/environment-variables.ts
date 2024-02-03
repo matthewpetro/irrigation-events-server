b057a4f445527e004aa731c4bfbbf4f00aecb28d
Matthew Petro <matthew.petro@aa.com>
2024-02-03 11:42:42 -0700

    Added definition for config variables

---
+++ src/environment-variables.ts
@@ -0,0 +1,10 @@
+export default interface EnvironmentVariables {
+  COUCHDB_URL: string
+  DB_NAME: string
+  DB_USERNAME: string
+  DB_PASSWORD: string
+  DB_AUTH_REFRESH_MINUTES: number
+  MAKER_API_URL: string
+  MAKER_API_ACCESS_TOKEN: string
+  RAIN_DELAY_DEVICE_ID: number
+}
