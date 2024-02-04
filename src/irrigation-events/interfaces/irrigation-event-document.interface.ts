3fd37211559cece091a9442dd17417a4f559cc10
Matthew Petro <matthew.petro@aa.com>
2024-02-03 20:06:33 -0700

    Moved DeviceState to enums directory

--- src/irrigation-events/interfaces/irrigation-event-document.interface.ts
+++ src/irrigation-events/interfaces/irrigation-event-document.interface.ts
@@ -1,5 +1,5 @@
 import { MaybeDocument } from 'nano'
-import { DeviceState } from './device-state.interface'
+import { DeviceState } from '../enums/device-state.interface'
 
 export interface IrrigationEventDocument extends MaybeDocument {
   _id: string
