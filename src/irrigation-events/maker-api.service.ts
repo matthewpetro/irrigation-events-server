f354cfed2dd420654c78d5225262a716383688c4
Matthew Petro <matthew.petro@aa.com>
2024-02-09 09:42:59 -0700

    Refactored a little for easier testing

--- src/irrigation-events/maker-api.service.ts
+++ src/irrigation-events/maker-api.service.ts
@@ -1,23 +1,25 @@
 import EnvironmentVariables from '@/environment-variables'
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import axios, { AxiosInstance } from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 import { MakerDeviceDetails } from './interfaces/maker-device-details.interface'
 import { DeviceStates } from './interfaces/device-states.interface'
 
 @Injectable()
 export class MakerApiService {
-  private axiosInstance: AxiosInstance
+  private axiosConfig: AxiosRequestConfig
   public constructor(private configService: ConfigService<EnvironmentVariables, true>) {
-    this.axiosInstance = axios.create({
+    this.axiosConfig = {
       baseURL: this.configService.get<string>('MAKER_API_URL', { infer: true }),
       params: {
         access_token: this.configService.get<string>('MAKER_API_ACCESS_TOKEN', { infer: true }),
       },
-    })
+    }
   }
-  public async getAllDeviceStates() {
-    const data = await this.axiosInstance.get<MakerDeviceDetails[]>('/all').then((response) => response.data)
+
+  public async getAllDeviceDetails() {
+    const response = await axios.get<MakerDeviceDetails[]>('/all', this.axiosConfig)
+    const data = response?.data ?? []
     return Object.fromEntries(data.map((device) => [parseInt(device.id, 10), device.attributes.switch])) as DeviceStates
   }
 }
