492d9cb39a11d089b6577c966c49d414cb7dcc0e
Matthew Petro <matthew.petro@aa.com>
2024-02-09 16:00:28 -0700

    Refactored to use IrrigationEvent domain object

---
+++ src/irrigation-events/domain/device-events.ts
@@ -0,0 +1,45 @@
+import { compareAsc } from 'date-fns'
+import { IrrigationEvent } from './irrigation-event'
+import { DeviceState } from '../enums/device-state.interface'
+
+const sortFn = (a: IrrigationEvent, b: IrrigationEvent) => compareAsc(a.getTimestamp(), b.getTimestamp())
+
+export class DeviceEvents {
+  private deviceId: number
+  private events: IrrigationEvent[] = []
+  private currentDeviceState?: DeviceState
+
+  constructor(deviceId: number, events: IrrigationEvent[]) {
+    this.deviceId = deviceId
+    this.events = [...events].sort(sortFn)
+  }
+
+  public getDeviceId(): number {
+    return this.deviceId
+  }
+
+  public getEvents(): IrrigationEvent[] {
+    return [...this.events]
+  }
+
+  public getFirstEvent(): IrrigationEvent {
+    return this.events[0]
+  }
+
+  public getLastEvent(): IrrigationEvent {
+    return this.events[this.events.length - 1]
+  }
+
+  public setCurrentDeviceState(state: DeviceState | undefined) {
+    this.currentDeviceState = state
+  }
+
+  public getCurrentDeviceState(): DeviceState | undefined {
+    return this.currentDeviceState
+  }
+
+  public addEvent(event: IrrigationEvent) {
+    this.events.push(event)
+    this.events.sort(sortFn)
+  }
+}
