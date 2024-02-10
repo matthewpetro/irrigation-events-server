0bfa06a4823becb40a4e321b7c1a81538b7a1399
Matthew Petro <matthew.petro@aa.com>
2024-02-10 14:03:17 -0700

    Got IrrigationEventsService tests working

--- src/irrigation-events/irrigation-events.service.spec.ts
+++ src/irrigation-events/irrigation-events.service.spec.ts
@@ -1,20 +1,71 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { IrrigationEventsService } from '@/irrigation-events/irrigation-events.service'
+import { IrrigationEventDocument } from './interfaces/irrigation-event-document.interface'
+import { MakerApiEventDto } from './dto/maker-api-event.dto'
+import { DeviceState } from './enums/device-state.interface'
 import { ConfigModule } from '@nestjs/config'
+
+// Mock the Nano library
+const mockInsert = jest.fn().mockResolvedValue({})
+const mockGet = jest.fn().mockResolvedValue({})
+jest.mock('nano', () => {
+  return {
+    __esModule: true,
+    default: jest.fn().mockImplementation(() => {
+      return {
+        auth: jest.fn(),
+        db: {
+          use: jest.fn().mockReturnValue({
+            get: mockGet,
+            insert: mockInsert,
+            // Add other methods as needed
+          }),
+        },
+      }
+    }),
+  }
+})
+
+import { IrrigationEventsService } from '@/irrigation-events/irrigation-events.service'
+
+const iso8601Regex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/
 
 describe('IrrigationEventsService', () => {
   let service: IrrigationEventsService
+  let testingModule: TestingModule
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      imports: [ConfigModule.forRoot({ envFilePath: '.env.local' })],
+    testingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot({ envFilePath: '.env.testing' })],
       providers: [IrrigationEventsService],
     }).compile()
 
-    service = module.get<IrrigationEventsService>(IrrigationEventsService)
+    testingModule.init()
+    testingModule.enableShutdownHooks()
+    service = testingModule.get<IrrigationEventsService>(IrrigationEventsService)
+  })
+
+  afterEach(async () => {
+    await testingModule.close()
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  it('should insert an irrigation event', async () => {
+    const mockIrrigationEvent: MakerApiEventDto = {
+      name: 'Name Field',
+      displayName: 'Display Name Field',
+      value: DeviceState.ON,
+      deviceId: 42,
+    }
+    const mockDocument: IrrigationEventDocument = {
+      _id: expect.stringMatching(iso8601Regex),
+      deviceName: 'Display Name Field',
+      state: DeviceState.ON,
+      deviceId: 42,
+    }
+    await service.insertIrrigationEvent(mockIrrigationEvent)
+    expect(mockInsert).toHaveBeenCalledWith(mockDocument)
   })
 })
