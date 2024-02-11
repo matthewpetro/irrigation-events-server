065dc08a1797988e87f4bdd9a3aa4be65271215a
Matthew Petro <matthew.petro@aa.com>
2024-02-10 22:01:23 -0700

    Added Nest devtools

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from '@/app.controller'
 import { AppService } from '@/app.service'
 import { ConfigModule } from '@nestjs/config'
 import { IrrigationEventsModule } from './irrigation-events/irrigation-events.module'
+import { DevtoolsModule } from '@nestjs/devtools-integration'
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: '.env.local' }), IrrigationEventsModule],
+  imports: [
+    DevtoolsModule.register({ http: process.env.NODE_ENV !== 'production' }),
+    ConfigModule.forRoot({ envFilePath: '.env.local' }),
+    IrrigationEventsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
