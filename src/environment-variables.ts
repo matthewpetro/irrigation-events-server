d149656216c5e73d909e7e9b52ec2798a530b3c8
Matthew Petro <matthew.petro@aa.com>
2024-02-12 18:47:22 -0700

    Updated database name variable name

--- src/environment-variables.ts
+++ src/environment-variables.ts
@@ -1,6 +1,6 @@
 export default interface EnvironmentVariables {
   COUCHDB_URL: string
-  DB_NAME: string
+  IRRIGATION_EVENTS_DB_NAME: string
   DB_USERNAME: string
   DB_PASSWORD: string
   DB_AUTH_REFRESH_MINUTES: number
