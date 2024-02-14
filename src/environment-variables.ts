dae80db3de1f71d3a9f4f57d3deb60baa0f21a0b
Matthew Petro <matthew.petro@aa.com>
2024-02-13 18:26:34 -0700

    Added environment variables

--- src/environment-variables.ts
+++ src/environment-variables.ts
@@ -1,9 +1,13 @@
 export default interface EnvironmentVariables {
   COUCHDB_URL: string
-  IRRIGATION_EVENTS_DB_NAME: string
   DB_USERNAME: string
   DB_PASSWORD: string
   DB_AUTH_REFRESH_MINUTES: number
   MAKER_API_URL: string
   MAKER_API_ACCESS_TOKEN: string
+  IRRIGATION_EVENTS_DB_NAME: string
+  SUNRISE_SUNSET_DB_NAME: string
+  SUNRISE_SUNSET_API_URL: string
+  LATITUDE: number
+  LONGITUDE: number
 }
