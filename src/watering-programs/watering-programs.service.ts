618347e117a53de7c430cbe6b316b9664de6b6ba
Matthew Petro <matthew.petro@aa.com>
2024-02-18 16:36:09 -0700

    Filled in functions with mock data

--- src/watering-programs/watering-programs.service.ts
+++ src/watering-programs/watering-programs.service.ts
@@ -1,26 +1,58 @@
 import { Injectable } from '@nestjs/common'
+import { v4 as uuidv4 } from 'uuid'
 import { CreateWateringProgramDto } from './dto/create-watering-program.dto'
 import { UpdateWateringProgramDto } from './dto/update-watering-program.dto'
+import { WateringProgramDto } from './dto/watering-program.dto'
 
 @Injectable()
 export class WateringProgramsService {
-  create(createWateringProgramDto: CreateWateringProgramDto) {
-    return 'This action adds a new wateringProgram'
+  async create(createWateringProgramDto: CreateWateringProgramDto) {
+    return { id: uuidv4(), ...createWateringProgramDto } as WateringProgramDto
   }
 
-  findAll() {
-    return `This action returns all wateringPrograms`
+  async findAll() {
+    return [
+      {
+        id: uuidv4(),
+        duration: 10,
+        wateringPeriod: 2,
+        startTime: '12:00:00Z',
+        switches: [1, 2],
+        simultaneousWatering: true,
+      } as WateringProgramDto,
+    ]
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} wateringProgram`
+  async findOne(id: string) {
+    return {
+      id,
+      duration: 10,
+      wateringPeriod: 2,
+      startTime: '12:00:00Z',
+      switches: [1, 2],
+      simultaneousWatering: true,
+    } as WateringProgramDto
   }
 
-  update(id: number, updateWateringProgramDto: UpdateWateringProgramDto) {
-    return `This action updates a #${id} wateringProgram`
+  async update(id: string, updateWateringProgramDto: UpdateWateringProgramDto) {
+    return {
+      id,
+      duration: updateWateringProgramDto.duration ?? 10,
+      wateringPeriod: updateWateringProgramDto.wateringPeriod ?? 2,
+      startTime: updateWateringProgramDto.startTime ?? '12:00:00Z',
+      switches: updateWateringProgramDto.switches ?? [1, 2],
+      simultaneousWatering: updateWateringProgramDto.simultaneousWatering ?? true,
+    } as WateringProgramDto
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} wateringProgram`
+  async remove(id: string) {
+    return {
+      id,
+      duration: 10,
+      wateringPeriod: 2,
+      startTime: '12:00:00Z',
+      switches: [1, 2],
+      simultaneousWatering: true,
+    } as WateringProgramDto
   }
 }
