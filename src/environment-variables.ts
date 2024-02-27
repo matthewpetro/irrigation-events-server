5f79c95e58cab82d7b6381d3b58158e7cbf31c86
Matthew Petro <matthew.petro@aa.com>
2024-02-27 08:13:25 -0700

    Add irrigation programs DB to env variables

--- src/environment-variables.ts
+++ src/environment-variables.ts
@@ -3,9 +3,14 @@ export default interface EnvironmentVariables {
   DB_USERNAME: string
   DB_PASSWORD: string
   DB_AUTH_REFRESH_MINUTES: number
+
   MAKER_API_URL: string
   MAKER_API_ACCESS_TOKEN: string
+
   IRRIGATION_EVENTS_DB_NAME: string
+
+  IRRIGATION_PROGRAMS_DB_NAME: string
+
   SUNRISE_SUNSET_DB_NAME: string
   SUNRISE_SUNSET_API_URL: string
   LATITUDE: number
