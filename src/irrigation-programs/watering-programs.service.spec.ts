9bd2ab32aebf807509420751313f20c064d4e2d8
Matthew Petro <matthew.petro@aa.com>
2024-02-27 08:22:21 -0700

    Fixed tests

--- src/irrigation-programs/watering-programs.service.spec.ts
+++ src/irrigation-programs/watering-programs.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { IrrigationProgramsService } from './watering-programs.service'
+import { DatabaseModule } from '@/database/database.module'
+import { ConfigModule } from '@nestjs/config'
 
 describe('IrrigationProgramsService', () => {
   let service: IrrigationProgramsService
@@ -7,6 +9,7 @@ describe('IrrigationProgramsService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [IrrigationProgramsService],
+      imports: [ConfigModule.forRoot({ envFilePath: '.env.testing' }), DatabaseModule],
     }).compile()
 
     service = module.get<IrrigationProgramsService>(IrrigationProgramsService)
