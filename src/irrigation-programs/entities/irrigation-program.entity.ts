e7c63615beb8cd25f3c8a6caa1813999d33732c2
Matthew Petro <matthew.petro@aa.com>
2024-02-28 12:51:22 -0700

    Coded irrigation progras service and unit tests

--- src/irrigation-programs/entities/irrigation-program.entity.ts
+++ src/irrigation-programs/entities/irrigation-program.entity.ts
@@ -1,13 +1,8 @@
-import { IdentifiedDocument } from 'nano'
-
-export class IrrigationProgram implements IdentifiedDocument {
-  constructor(
-    public _id: string,
-    public duration: number,
-    public wateringPeriod: number,
-    public startTime: string,
-    public switches: number[],
-    public simultaneousIrrigation: boolean,
-    public nextRunDate?: string
-  ) {}
+export interface IrrigationProgramEntity {
+  duration: number
+  wateringPeriod: number
+  startTime: string
+  switches: number[]
+  simultaneousIrrigation: boolean
+  nextRunDate?: string
 }
--- src/irrigation-programs/irrigation-programs.service.ts
+++ src/irrigation-programs/irrigation-programs.service.ts
@@ -1,17 +1,39 @@
 import { Injectable, OnModuleInit } from '@nestjs/common'
-import { v4 as uuidv4 } from 'uuid'
 import { CreateIrrigationProgramDto } from './dto/create-irrigation-program.dto'
 import { UpdateIrrigationProgramDto } from './dto/update-irrigation-program.dto'
 import { IrrigationProgramDto } from './dto/irrigation-program.dto'
-import { IrrigationProgram } from './entities/irrigation-program.entity'
+import { IrrigationProgramEntity } from './entities/irrigation-program.entity'
 import { ConfigService } from '@nestjs/config'
 import EnvironmentVariables from '@/environment-variables'
 import { DatabaseService } from '@/database/database.service'
-import { DocumentScope } from 'nano'
+import { DocumentScope, IdentifiedDocument, MaybeDocument } from 'nano'
+
+type IrrigationProgramDocument = IrrigationProgramEntity & IdentifiedDocument
+
+type IrrigationProgramMaybeDocument = IrrigationProgramEntity & MaybeDocument
+
+const irrigationDocumentToDto = (irrigationProgram: IrrigationProgramDocument): IrrigationProgramDto => ({
+  id: irrigationProgram._id,
+  duration: irrigationProgram.duration,
+  wateringPeriod: irrigationProgram.wateringPeriod,
+  startTime: irrigationProgram.startTime,
+  switches: irrigationProgram.switches,
+  simultaneousIrrigation: irrigationProgram.simultaneousIrrigation,
+  nextRunDate: irrigationProgram.nextRunDate,
+})
+
+const creationDtoToDocument = (irrigationProgramDto: CreateIrrigationProgramDto): IrrigationProgramMaybeDocument => ({
+  duration: irrigationProgramDto.duration,
+  wateringPeriod: irrigationProgramDto.wateringPeriod,
+  startTime: irrigationProgramDto.startTime,
+  switches: irrigationProgramDto.switches,
+  simultaneousIrrigation: irrigationProgramDto.simultaneousIrrigation,
+  nextRunDate: irrigationProgramDto.nextRunDate,
+})
 
 @Injectable()
 export class IrrigationProgramsService implements OnModuleInit {
-  private db: DocumentScope<IrrigationProgram>
+  private db: DocumentScope<IrrigationProgramMaybeDocument>
 
   public constructor(
     private configService: ConfigService<EnvironmentVariables, true>,
@@ -20,68 +42,41 @@ export class IrrigationProgramsService implements OnModuleInit {
 
   onModuleInit() {
     this.db = this.databaseService.getDatabaseConnection(
-      this.configService.get<IrrigationProgram>('IRRIGATION_PROGRAMS_DB_NAME', { infer: true })
+      this.configService.get('IRRIGATION_PROGRAMS_DB_NAME', { infer: true })
     )
   }
 
   async create(createIrrigationProgramDto: CreateIrrigationProgramDto) {
-    const uuid = uuidv4()
-    const irrigationProgram = new IrrigationProgram(
-      uuid,
-      createIrrigationProgramDto.duration,
-      createIrrigationProgramDto.wateringPeriod,
-      createIrrigationProgramDto.startTime,
-      createIrrigationProgramDto.switches,
-      createIrrigationProgramDto.simultaneousIrrigation,
-      createIrrigationProgramDto.nextRunDate
-    )
-    await this.db.insert(irrigationProgram)
-    return { id: uuid, ...createIrrigationProgramDto } as IrrigationProgramDto
+    const result = await this.db.insert(creationDtoToDocument(createIrrigationProgramDto))
+    return result.ok ? ({ id: result.id, ...createIrrigationProgramDto } as IrrigationProgramDto) : null
   }
 
   async findAll() {
-    return [
-      {
-        id: uuidv4(),
-        duration: 10,
-        wateringPeriod: 2,
-        startTime: '12:00:00Z',
-        switches: [1, 2],
-        simultaneousIrrigation: true,
-      } as IrrigationProgramDto,
-    ]
+    const result = await this.db.list({ include_docs: true })
+    return result.rows.map((row) => irrigationDocumentToDto(row.doc!))
   }
 
   async findOne(id: string) {
-    return {
-      id,
-      duration: 10,
-      wateringPeriod: 2,
-      startTime: '12:00:00Z',
-      switches: [1, 2],
-      simultaneousIrrigation: true,
-    } as IrrigationProgramDto
+    const result = await this.db.get(id)
+    return result && !result._deleted ? irrigationDocumentToDto(result) : null
   }
 
   async update(id: string, updateIrrigationProgramDto: UpdateIrrigationProgramDto) {
-    return {
-      id,
-      duration: updateIrrigationProgramDto.duration ?? 10,
-      wateringPeriod: updateIrrigationProgramDto.wateringPeriod ?? 2,
-      startTime: updateIrrigationProgramDto.startTime ?? '12:00:00Z',
-      switches: updateIrrigationProgramDto.switches ?? [1, 2],
-      simultaneousIrrigation: updateIrrigationProgramDto.simultaneousIrrigation ?? true,
-    } as IrrigationProgramDto
+    const currentDocument = await this.db.get(id)
+    if (!currentDocument || currentDocument._deleted) {
+      return null
+    }
+    const newDocument: IrrigationProgramDocument = {
+      ...currentDocument,
+      ...updateIrrigationProgramDto,
+    }
+    const result = await this.db.insert(newDocument)
+    return result.ok ? irrigationDocumentToDto(newDocument) : null
   }
 
   async remove(id: string) {
-    return {
-      id,
-      duration: 10,
-      wateringPeriod: 2,
-      startTime: '12:00:00Z',
-      switches: [1, 2],
-      simultaneousIrrigation: true,
-    } as IrrigationProgramDto
+    const { etag: revision } = await this.db.head(id)
+    const result = await this.db.destroy(id, revision)
+    return result.ok
   }
 }
