5b18ba397fd82c21418a7b1f80d9b0eb77ee1d51
Matthew Petro <matthew.petro@aa.com>
2024-02-28 12:51:22 -0700

    Coded irrigation programs service and unit tests

--- src/irrigation-programs/irrigation-programs.controller.ts
+++ src/irrigation-programs/irrigation-programs.controller.ts
@@ -1,15 +1,33 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from '@nestjs/common'
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseUUIDPipe,
+  HttpException,
+  HttpStatus,
+  ValidationPipe,
+  UsePipes,
+} from '@nestjs/common'
 import { IrrigationProgramsService } from './irrigation-programs.service'
 import { CreateIrrigationProgramDto } from './dto/create-irrigation-program.dto'
 import { UpdateIrrigationProgramDto } from './dto/update-irrigation-program.dto'
 
 @Controller('irrigation-programs')
+@UsePipes(new ValidationPipe({ whitelist: true, transform: true }))
 export class IrrigationProgramsController {
   constructor(private readonly irrigationProgramsService: IrrigationProgramsService) {}
 
   @Post()
   async create(@Body() createIrrigationProgramDto: CreateIrrigationProgramDto) {
-    return this.irrigationProgramsService.create(createIrrigationProgramDto)
+    const result = this.irrigationProgramsService.create(createIrrigationProgramDto)
+    if (result === null) {
+      throw new HttpException('Failed to create irrigation program', HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+    return result
   }
 
   @Get()
@@ -19,7 +37,11 @@ export class IrrigationProgramsController {
 
   @Get(':id')
   async findOne(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
-    return this.irrigationProgramsService.findOne(id)
+    const irrigationProgram = await this.irrigationProgramsService.findOne(id)
+    if (irrigationProgram === null) {
+      throw new HttpException(`Irrigation program with ID ${id} not found`, HttpStatus.NOT_FOUND)
+    }
+    return irrigationProgram
   }
 
   @Patch(':id')
@@ -27,7 +49,11 @@ export class IrrigationProgramsController {
     @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
     @Body() updateIrrigationProgramDto: UpdateIrrigationProgramDto
   ) {
-    return this.irrigationProgramsService.update(id, updateIrrigationProgramDto)
+    const irrigationProgram = this.irrigationProgramsService.update(id, updateIrrigationProgramDto)
+    if (irrigationProgram === null) {
+      throw new HttpException(`Irrigation program with ID ${id} not found`, HttpStatus.NOT_FOUND)
+    }
+    return irrigationProgram
   }
 
   @Delete(':id')
--- src/irrigation-programs/irrigation-programs.service.spec.ts
+++ src/irrigation-programs/irrigation-programs.service.spec.ts
@@ -2,10 +2,19 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { IrrigationProgramsService } from './irrigation-programs.service'
 import { DatabaseModule } from '@/database/database.module'
 import { ConfigModule } from '@nestjs/config'
+import { v4 as uuidv4 } from 'uuid'
+import { IrrigationProgramEntity } from './entities/irrigation-program.entity'
+import { IrrigationProgramDto } from './dto/irrigation-program.dto'
+import { CreateIrrigationProgramDto } from './dto/create-irrigation-program.dto'
+import { UpdateIrrigationProgramDto } from './dto/update-irrigation-program.dto'
 
 // Mock the Nano library
-const mockInsert = jest.fn()
+const mockDestroy = jest.fn()
 const mockFind = jest.fn()
+const mockGet = jest.fn()
+const mockHead = jest.fn()
+const mockInsert = jest.fn()
+const mockList = jest.fn()
 jest.mock('nano', () => {
   return {
     __esModule: true,
@@ -14,8 +23,12 @@ jest.mock('nano', () => {
         auth: jest.fn(),
         db: {
           use: jest.fn().mockReturnValue({
+            destroy: mockDestroy,
             find: mockFind,
+            get: mockGet,
+            head: mockHead,
             insert: mockInsert,
+            list: mockList,
           }),
         },
       }
@@ -48,13 +61,178 @@ describe('IrrigationProgramsService', () => {
   })
 
   it('should create an irrigation program', async () => {
-    const irrigationProgram = await service.create({
+    mockInsert.mockResolvedValue({ id: uuidv4(), ok: true })
+    const mockCreateDto: CreateIrrigationProgramDto = {
       duration: 10,
       wateringPeriod: 2,
       startTime: '12:00:00Z',
       switches: [1, 2],
       simultaneousIrrigation: true,
+    }
+    const irrigationProgram = await service.create(mockCreateDto)
+    expect(mockInsert).toHaveBeenCalledWith(mockCreateDto)
+    expect(irrigationProgram).toEqual({
+      id: expect.stringMatching(/[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/i),
+      ...mockCreateDto,
     })
-    expect(irrigationProgram).toBeDefined()
+  })
+
+  it('should not create an irrigation program', async () => {
+    mockInsert.mockResolvedValue({ ok: false })
+    const mockCreateDto: CreateIrrigationProgramDto = {
+      duration: 10,
+      wateringPeriod: 2,
+      startTime: '12:00:00Z',
+      switches: [1, 2],
+      simultaneousIrrigation: true,
+    }
+    const irrigationProgram = await service.create(mockCreateDto)
+    expect(mockInsert).toHaveBeenCalledWith(mockCreateDto)
+    expect(irrigationProgram).toBeNull()
+  })
+
+  it('should find all irrigation programs', async () => {
+    const mockIrrigationProgram1 = {
+      duration: 10,
+      wateringPeriod: 2,
+      startTime: '12:00:00Z',
+      switches: [1, 2],
+      simultaneousIrrigation: true,
+    }
+    const mockIrrigationProgram2 = {
+      duration: 20,
+      wateringPeriod: 4,
+      startTime: '05:00:00Z',
+      switches: [3],
+      simultaneousIrrigation: false,
+    }
+    const uuid1 = uuidv4()
+    const uuid2 = uuidv4()
+    const mockListRepsonse = {
+      rows: [{ doc: { _id: uuid1, ...mockIrrigationProgram1 } }, { doc: { _id: uuid2, ...mockIrrigationProgram2 } }],
+    }
+    mockList.mockResolvedValue(mockListRepsonse)
+    const irrigationPrograms = await service.findAll()
+    expect(mockList).toHaveBeenCalled()
+    expect(irrigationPrograms).toEqual([
+      { id: uuid1, ...mockIrrigationProgram1 },
+      { id: uuid2, ...mockIrrigationProgram2 },
+    ] as IrrigationProgramDto[])
+  })
+
+  it('should not find any irrigation programs', async () => {
+    mockList.mockResolvedValue({ rows: [] })
+    const irrigationPrograms = await service.findAll()
+    expect(mockList).toHaveBeenCalled()
+    expect(irrigationPrograms).toEqual([])
+  })
+
+  it('should find one irrigation program', async () => {
+    const id = uuidv4()
+    const mockPartialIrrigationProgram = {
+      duration: 10,
+      wateringPeriod: 2,
+      startTime: '12:00:00Z',
+      switches: [1, 2],
+      simultaneousIrrigation: true,
+    }
+    mockGet.mockResolvedValue({ _id: id, ...mockPartialIrrigationProgram } as IrrigationProgramEntity)
+    const irrigationProgram = await service.findOne(id)
+    expect(mockGet).toHaveBeenCalledWith(id)
+    expect(irrigationProgram).toEqual({
+      id: id,
+      ...mockPartialIrrigationProgram,
+    } as IrrigationProgramDto)
+  })
+
+  it('should not find an irrigation program', async () => {
+    const id = uuidv4()
+    mockGet.mockResolvedValue({ _deleted: true })
+    const irrigationProgram = await service.findOne(id)
+    expect(mockGet).toHaveBeenCalledWith(id)
+    expect(irrigationProgram).toBeNull()
+  })
+
+  it('should update an irrigation program', async () => {
+    const id = uuidv4()
+    const rev = '1-234'
+    const mockIrrigationProgram: IrrigationProgramEntity = {
+      duration: 10,
+      wateringPeriod: 2,
+      startTime: '12:00:00Z',
+      switches: [1, 2],
+      simultaneousIrrigation: true,
+    }
+    mockGet.mockResolvedValue({ _id: id, _rev: rev, ...mockIrrigationProgram })
+    mockInsert.mockResolvedValue({ ok: true })
+    const mockUpdateDto: UpdateIrrigationProgramDto = {
+      duration: 5,
+      wateringPeriod: 1,
+    }
+    const irrigationProgram = await service.update(id, mockUpdateDto)
+    expect(mockGet).toHaveBeenCalledWith(id)
+    expect(mockInsert).toHaveBeenCalledWith({ _id: id, _rev: rev, ...mockIrrigationProgram, ...mockUpdateDto })
+    expect(irrigationProgram).toEqual({
+      id,
+      ...mockIrrigationProgram,
+      ...mockUpdateDto,
+    } as IrrigationProgramDto)
+  })
+
+  it('should not update an irrigation program because the program has been deleted', async () => {
+    const id = uuidv4()
+    mockGet.mockResolvedValue({ _deleted: true })
+    const mockUpdateDto: UpdateIrrigationProgramDto = {
+      duration: 5,
+      wateringPeriod: 1,
+    }
+    const irrigationProgram = await service.update(id, mockUpdateDto)
+    expect(mockGet).toHaveBeenCalledWith(id)
+    expect(mockInsert).not.toHaveBeenCalled()
+    expect(irrigationProgram).toBeNull()
+  })
+
+  it('should not update an irrigation program because the update failed', async () => {
+    const id = uuidv4()
+    const rev = '1-234'
+    const mockIrrigationProgram: IrrigationProgramEntity = {
+      duration: 10,
+      wateringPeriod: 2,
+      startTime: '12:00:00Z',
+      switches: [1, 2],
+      simultaneousIrrigation: true,
+    }
+    mockGet.mockResolvedValue({ _id: id, _rev: rev, ...mockIrrigationProgram })
+    mockInsert.mockResolvedValue({ ok: false })
+    const mockUpdateDto: UpdateIrrigationProgramDto = {
+      duration: 5,
+      wateringPeriod: 1,
+    }
+    const irrigationProgram = await service.update(id, mockUpdateDto)
+    expect(mockGet).toHaveBeenCalledWith(id)
+    expect(mockInsert).toHaveBeenCalledWith({ _id: id, _rev: rev, ...mockIrrigationProgram, ...mockUpdateDto })
+    expect(irrigationProgram).toBeNull()
+  })
+
+  it('should remove an irrigation program', async () => {
+    const id = uuidv4()
+    const rev = '1-234'
+    mockHead.mockResolvedValue({ etag: rev })
+    mockDestroy.mockResolvedValue({ ok: true, id, rev })
+    const result = await service.remove(id)
+    expect(mockHead).toHaveBeenCalledWith(id)
+    expect(mockDestroy).toHaveBeenCalledWith(id, rev)
+    expect(result).toBeTruthy()
+  })
+
+  it('should not remove an irrigation program because deltion failed', async () => {
+    const id = uuidv4()
+    const rev = '1-234'
+    mockHead.mockResolvedValue({ etag: rev })
+    mockDestroy.mockResolvedValue({ ok: false })
+    const result = await service.remove(id)
+    expect(mockHead).toHaveBeenCalledWith(id)
+    expect(mockDestroy).toHaveBeenCalledWith(id, rev)
+    expect(result).toBeFalsy()
   })
 })
