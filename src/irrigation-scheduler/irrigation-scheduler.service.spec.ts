013458751fe29be5a451a8418e2d982affca7649
Matthew Petro <matthew.petro@aa.com>
2024-03-16 23:12:03 -0700

    Got irrigation scheduler basic unit test working

--- src/irrigation-scheduler/irrigation-scheduler.service.spec.ts
+++ src/irrigation-scheduler/irrigation-scheduler.service.spec.ts
@@ -1,15 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { IrrigationSchedulerService } from './irrigation-scheduler.service'
+import { IrrigationProgramsModule } from '@/irrigation-programs/irrigation-programs.module'
+import { MakerApiModule } from '@/maker-api/maker-api.module'
+import { SunriseSunsetModule } from '@/sunrise-sunset/sunrise-sunset.module'
 
-xdescribe('IrrigationSchedulerService', () => {
+describe('IrrigationSchedulerService', () => {
   let service: IrrigationSchedulerService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [IrrigationSchedulerService],
+      imports: [IrrigationProgramsModule, MakerApiModule, SunriseSunsetModule],
     }).compile()
 
     service = module.get<IrrigationSchedulerService>(IrrigationSchedulerService)
+  })
+
+  afterEach(async () => {
+    jest.clearAllMocks()
   })
 
   it('should be defined', () => {
