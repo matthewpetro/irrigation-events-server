02bfac6c741143dd65f3668bb2e1ebca4b419f73
Matthew Petro <matthew.petro@aa.com>
2024-03-17 21:50:37 -0700

    Started setting up mocks

--- src/irrigation-scheduler/irrigation-scheduler.service.spec.ts
+++ src/irrigation-scheduler/irrigation-scheduler.service.spec.ts
@@ -4,6 +4,22 @@ import { IrrigationProgramsModule } from '@/irrigation-programs/irrigation-progr
 import { MakerApiModule } from '@/maker-api/maker-api.module'
 import { SunriseSunsetModule } from '@/sunrise-sunset/sunrise-sunset.module'
 import { ConfigModule } from '@nestjs/config'
+import { IrrigationProgramsService } from '@/irrigation-programs/irrigation-programs.service'
+import { MakerApiService } from '@/maker-api/maker-api.service'
+import { SunriseSunsetService } from '@/sunrise-sunset/sunrise-sunset.service'
+
+const mockIrrigationProgramsService = {
+  findAll: jest.fn(),
+  update: jest.fn(),
+}
+
+const mockMakerApiService = {
+  setDeviceState: jest.fn(),
+}
+
+const mockSunriseSunsetService = {
+  getSunriseSunset: jest.fn(),
+}
 
 describe('IrrigationSchedulerService', () => {
   let service: IrrigationSchedulerService
@@ -17,16 +33,45 @@ describe('IrrigationSchedulerService', () => {
         MakerApiModule,
         SunriseSunsetModule,
       ],
-    }).compile()
-
+    })
+      .useMocker((token) => {
+        if (token === IrrigationProgramsService) {
+          return mockIrrigationProgramsService
+        }
+        if (token === MakerApiService) {
+          return mockMakerApiService
+        }
+        if (token === SunriseSunsetService) {
+          return mockSunriseSunsetService
+        }
+      })
+      .compile()
     service = module.get<IrrigationSchedulerService>(IrrigationSchedulerService)
   })
 
-  afterEach(async () => {
+  afterEach(() => {
     jest.clearAllMocks()
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  it('should do nothing if no programs are running or need to start', () => {})
+
+  describe('program with one device', () => {
+    it('should start the program', () => {})
+    it('should stop the program', () => {})
+  })
+
+  describe('program with multiple devices and simultaneous irrigation set to true', () => {
+    it('should start the program', () => {})
+    it('should stop the program', () => {})
+  })
+
+  describe('program with multiple devices and simultaneous irrigation set to false', () => {
+    it('should start the program', () => {})
+    it('should turn off one device and turn another on', () => {})
+    it('should stop the program', () => {})
   })
 })
