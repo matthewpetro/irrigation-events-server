3d9c8d516c04dc01bcd8b9ec1dc9c926889f7580
Matthew Petro <matthew.petro@aa.com>
2024-03-18 21:02:54 -0700

    Started adding tests for irrigation scheduler service

--- src/irrigation-scheduler/irrigation-scheduler.service.spec.ts
+++ src/irrigation-scheduler/irrigation-scheduler.service.spec.ts
@@ -7,6 +7,8 @@ import { ConfigModule } from '@nestjs/config'
 import { IrrigationProgramsService } from '@/irrigation-programs/irrigation-programs.service'
 import { MakerApiService } from '@/maker-api/maker-api.service'
 import { SunriseSunsetService } from '@/sunrise-sunset/sunrise-sunset.service'
+import { addDays, format, parseISO, subDays } from 'date-fns'
+import { v4 as uuidv4 } from 'uuid'
 
 const mockIrrigationProgramsService = {
   findAll: jest.fn(),
@@ -57,7 +59,65 @@ describe('IrrigationSchedulerService', () => {
     expect(service).toBeDefined()
   })
 
-  it('should do nothing if no programs are running or need to start', () => {})
+  it('should do nothing if no programs are running or need to start', () => {
+    mockSunriseSunsetService.getSunriseSunset.mockResolvedValue({
+      sunrise: parseISO('2024-02-12T07:15:32-07:00'),
+      sunset: parseISO('2024-02-12T18:12:30-07:00'),
+    })
+    mockIrrigationProgramsService.findAll.mockResolvedValue([
+      // Next run date is in the future
+      {
+        id: uuidv4(),
+        name: 'Test 1',
+        duration: 5,
+        wateringPeriod: 2,
+        startTime: '06:00',
+        deviceIds: [1],
+        simultaneousIrrigation: false,
+        nextRunDate: format(addDays(new Date(), 1), 'yyyy-MM-dd'),
+      },
+      // No next run date
+      {
+        id: uuidv4(),
+        name: 'Test 2',
+        duration: 10,
+        wateringPeriod: 3,
+        startTime: '07:00',
+        deviceIds: [2],
+        simultaneousIrrigation: false,
+      },
+      // Next run date is today
+      {
+        id: uuidv4(),
+        name: 'Test 3',
+        duration: 10,
+        wateringPeriod: 3,
+        startTime: '07:00',
+        deviceIds: [3],
+        simultaneousIrrigation: false,
+        nextRunDate: format(new Date(), 'yyyy-MM-dd'),
+      },
+      // Next run date is in the past
+      {
+        id: uuidv4(),
+        name: 'Test 4',
+        duration: 10,
+        wateringPeriod: 3,
+        startTime: '07:00',
+        deviceIds: [4],
+        simultaneousIrrigation: false,
+        nextRunDate: format(subDays(new Date(), 1), 'yyyy-MM-dd'),
+      },
+    ])
+  })
+
+  it('should correctly calculate the next run date', () => {})
+
+  describe('start time conversions', () => {
+    it('should correctly convert a start time  to actual time', () => {})
+    it('should correctly convert sunrise start time to actual time', () => {})
+    it('should correctly convert sunset start time to actual time', () => {})
+  })
 
   describe('program with one device', () => {
     it('should start the program', () => {})
