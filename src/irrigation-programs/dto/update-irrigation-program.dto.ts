bedce64c5e738f59b2e527b5208a64a0180eb823
Matthew Petro <matthew.petro@aa.com>
2024-04-15 09:44:46 -0700

    Use PartialType from @nestjs/swagger library

--- src/irrigation-programs/dto/update-irrigation-program.dto.ts
+++ src/irrigation-programs/dto/update-irrigation-program.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types'
+import { PartialType } from '@nestjs/swagger'
 import { CreateIrrigationProgramDto } from './create-irrigation-program.dto'
 
 export class UpdateIrrigationProgramDto extends PartialType(CreateIrrigationProgramDto) {}
