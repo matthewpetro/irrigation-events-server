99d960de80731ef5587e38dd532eee067a0b50a8
Matthew Petro <matt@petrofamily.com>
2025-01-04 20:42:10 -0700

    Use a guard to block controller in prod environment

--- src/sunrise-sunset/sunrise-sunset.controller.ts
+++ src/sunrise-sunset/sunrise-sunset.controller.ts
@@ -1,10 +1,15 @@
 /* eslint-disable max-classes-per-file */
-import { Controller, Get, Query } from '@nestjs/common'
+import { CanActivate, Controller, Get, Injectable, Query, UseGuards } from '@nestjs/common'
 import { IsISO8601 } from 'class-validator'
 import { parseISO } from 'date-fns'
 import { SunriseSunsetService } from './sunrise-sunset.service'
 
-// TODO: Remove this controller when no longer needed for testing purposes
+// Use a guard to prevent this controller from being called in a non-development environment
+@Injectable()
+class DevelopmentGuard implements CanActivate {
+  canActivate = () => process.env.NODE_ENV === 'development'
+}
+
 class QueryParameters {
   @IsISO8601()
   startDate: string
@@ -13,7 +18,9 @@ class QueryParameters {
   endDate: string
 }
 
+// TODO: Remove this controller when no longer needed for testing purposes
 @Controller('sunrise-sunset')
+@UseGuards(DevelopmentGuard)
 export class SunriseSunsetController {
   constructor(private sunriseSunsetService: SunriseSunsetService) {}
 
