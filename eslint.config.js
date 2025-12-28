63e05c28384d617fc21fb75d8460babac336afac
Matthew Petro <matt@petrofamily.com>
2025-12-27 20:30:00 -0700

    Updated to new ESLint config

---
+++ eslint.config.js
@@ -0,0 +1,65 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import js from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import globals from 'globals';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+export default [
+  {
+    ignores: ['.eslintrc.js', 'eslint.config.js', 'dist/**', 'node_modules/**'],
+  },
+  js.configs.recommended,
+  ...compat.extends('airbnb-base', 'prettier'),
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: './tsconfig.json',
+        },
+      },
+    },
+    rules: {
+      // Apply TypeScript ESLint recommended rules
+      ...tsPlugin.configs.recommended.rules,
+      
+      // Custom overrides
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'class-methods-use-this': 'off',
+      'import/prefer-default-export': 'off',
+      'import/no-default-export': 'error',
+      'import/extensions': 'off',
+      'no-underscore-dangle': ['error', { allow: ['_id', '_rev', '_deleted'] }],
+      'radix': 'off',
+    },
+  },
+];
